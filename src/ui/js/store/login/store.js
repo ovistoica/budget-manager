50d3a85168bd479d28c26025fbdb68056a6291d8
Stoica Ovidiu <ovytkl@gmail.com>
2019-01-31 20:44:54 +0200

    Login vue modules

---
+++ src/ui/js/store/login/store.js
@@ -0,0 +1,20 @@
+'use strict';
+let Vue = require('vue');
+let VueResource = require('vue-resource');
+let Vuex = require('vuex');
+let settings = require('../modules/settings');
+let user = require('../modules/user');
+Vue.use(VueResource);
+Vue.use(Vuex);
+
+module.exports = new Vuex.Store({
+	modules: {
+		user,
+		settings
+	},
+	strict: process.env.NODE_ENV !== 'production'
+});
+
+
+
+
---
+++ src/ui/js/store/modules/settings/index.js
@@ -0,0 +1,71 @@
+'use strict';
+let Vue = require('vue');
+let _ = require('lodash');
+let setup = require('../setup');
+
+module.exports = {
+	namespaced: true,
+	state : {
+		NAME: 'Budget Manager',
+		LOGIN: '/login.html',
+		ERROR: './error.html',
+		//TODO: Other properties here
+		roles: [
+			{
+				name: 'user',
+				title: 'User'
+			},
+			{
+				name: 'admin',
+				title: 'Administrator'
+			}
+		]
+
+	},
+	getters: {
+		NAME (state)
+		{
+			return state.NAME;
+		},
+		ROLES (state)
+		{
+			return state.ROLES;
+		}
+	},
+	actions: {
+		async init (store)
+		{
+			try
+			{
+				let response = await Vue.http.get (setup.API+'/settings');
+				console.log (response);	
+				store.commit ('init', response.data);
+				
+			}
+			catch (e)
+			{
+				store.dispatch ('redirect', 'ERROR');
+			}
+		},
+		redirect (store, application)
+		{
+			let address = store.state[application];
+			console.log (address);
+			if (address !== '' && address !== undefined && address !== null)
+			{
+				console.log ('Redirect to '+address);
+				window.location.href = address;
+			}
+		}
+	},
+	mutations: {
+		init (state, settings)
+		{
+			// if (settings.WORKSPACE.indexOf ('http')!==0) settings.WORKSPACE = location.protocol+'//'+settings.WORKSPACE;
+			_.assign (state, settings);
+		}
+	}
+
+
+
+};
